620200fd759d06e5fe3db38468dc75225a4a42fd
robin <hoffmann.rob@gmx.de>
2020-07-06 10:41:07 +0200

    ReadME angepasst und docker fixes

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 # WebShopDocker
 
-start backend:
-- cd to backend folder
-- brew install mongodb-community (once)
-- docker-compose up
-- npm install
-- npm run seed
-- npm run start
-
-start frontend:
-- cd to frontend folder
-- brew install angular-cli (once)
-- docker-compose up
-- ng serve
+docker-compose up -d --build
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express'),
+
 	mongoose = require('mongoose'),
 	bodyParser = require('body-parser'),
 	cookieParser = require('cookie-parser'),
     methodOverride = require('method-override'),
 	cors = require('cors'),
 	app = express();
+
+
 
 const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
 	envConfig = require('./server/env')[env];
@@ -23,6 +26,19 @@ app.use(express.static(__dirname + '/public'));
 
 require('./server/routes')(app);
 
+// Handling CORS
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	if (req.method === "OPTIONS") {
+		// Set allowed HTTP Methods
+		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+		return res.status(204);
+	}
+	next();
+});
+
 app.listen(envConfig.port, function(){
   console.log('Server listening on port ' + envConfig.port)
 });
+
